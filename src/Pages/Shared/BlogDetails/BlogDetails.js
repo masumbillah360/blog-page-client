ab4e954bb60656295cc5d54889eeb8009b11010e
Masum Billah <masumbillah6584@gmail.com>
2022-11-20 23:06:47 +0600

    added comments section and show comments

--- src/Pages/Shared/BlogDetails/BlogDetails.js
+++ src/Pages/Shared/BlogDetails/BlogDetails.js
@@ -7,7 +7,7 @@ const BlogDetails = () => {
   const { name, picture, description } = blogData;
   return (
     <PhotoProvider>
-      <div className="">
+      <div className="px-2 md:p-0">
         <div>
           <PhotoView src={picture}>
             <img
@@ -21,8 +21,64 @@ const BlogDetails = () => {
             <p>{description}</p>
           </div>
         </div>
-        <div>
-            
+        <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="w-full text-center">
+            <textarea
+              className="textarea textarea-primary w-4/5 md:w-full px-1"
+              placeholder="Bio"
+            />
+            <div className="my-2 text-center md:text-end">
+                
+            <button className="btn btn-primary">Add</button>
+            </div>
+          </div>
+          <div className=" w-full grid grid-cols-1 gap-4 max-h-screen overflow-auto">
+            <div className="flex justify-start items-start w-full">
+              <div className="avatar mx-4">
+                <div className="w-12 mt-1 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src="https://placeimg.com/192/192/people" alt="" />
+                </div>
+              </div>
+              <div>
+                <h4 className="text-xl font-bold">Md: Masum Billah</h4>
+                <p>
+                  Die seh mein um zauberhauch euch gleich nicht. Wie sonst ich
+                  an bilder ich, unbestimmten irren ich mit wiederholt mein. Ein
+                  einer zug ich steigen wohl es freundschaft froher fühl,.
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-start items-start">
+              <div className="avatar mx-4">
+                <div className="w-12 mt-1 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src="https://placeimg.com/192/192/people" alt="" />
+                </div>
+              </div>
+              <div>
+                <h4 className="text-xl font-bold">Md: Masum Billah</h4>
+                <p>
+                  Die seh mein um zauberhauch euch gleich nicht. Wie sonst ich
+                  an bilder ich, unbestimmten irren ich mit wiederholt mein. Ein
+                  einer zug ich steigen wohl es freundschaft froher fühl,.
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-start items-start">
+              <div className="avatar mx-4">
+                <div className="w-12 mt-1 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src="https://placeimg.com/192/192/people" alt="" />
+                </div>
+              </div>
+              <div>
+                <h4 className="text-xl font-bold">Md: Masum Billah</h4>
+                <p>
+                  Die seh mein um zauberhauch euch gleich nicht. Wie sonst ich
+                  an bilder ich, unbestimmten irren ich mit wiederholt mein. Ein
+                  einer zug ich steigen wohl es freundschaft froher fühl,.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </PhotoProvider>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-neutral text-neutral-content">
+    <footer className="footer mt-10 rounded-lg p-10 bg-neutral text-neutral-content">
       <div>
         <svg
           width="50"
