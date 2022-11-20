aa1ed912f1ce2004121ffc57b8117c37132be8ab
Masum Billah <masumbillah6584@gmail.com>
2022-11-20 19:30:42 +0600

    added social login btn and icons

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,67 @@
 import React from "react";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
-  return <div>login Page</div>;
+  return (
+    <div className="grid grid-cols-12 my-24 md:gap-5 justify-center items-center">
+      <div className="col-span-12 md:col-span-4">
+        <h1 className="text-5xl font-bold text-center md:text-end">
+          Let's Play The Life Changing Game
+        </h1>
+        <p className="text-center md:text-end underline mt-5 font-bold text-green-500">
+          Don't Hide Your Creativity From Other's
+        </p>
+      </div>
+      <div className="col-span-12 md:col-span-8">
+        <div className="card w-full shadow-2xl bg-base-100">
+          <div className="card-body w-full">
+            <div className="flex justify-around w-full">
+              <button className="btn rounded-lg text-4xl btn-primary my-1">
+                <FcGoogle></FcGoogle>{" "}
+              </button>
+              <button className="btn rounded-lg text-4xl btn-primary my-1">
+                <FaFacebook />
+              </button>
+              <button className="btn  rounded-lg text-4xl btn-primary my-1">
+                <FaTwitter />
+              </button>
+            </div>
+            <form>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered w-full"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
